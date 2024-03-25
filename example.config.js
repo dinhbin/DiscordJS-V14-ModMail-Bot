2dd2503efe35ce18d096120c0a2c0a13998ffcfa
dinhbin <152063864+dinhbin@users.noreply.github.com>
2024-03-26 00:00:15 +0700

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: {
-        token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        token: 'MTIyMTgzODk2ODU3Mjc0Mzg0MA.G1ixvl.WC4YDxUpqMAz7X7Cn4SbNhnv2P0_h_PXjWCjMA', // ← Your bot token (.env IS RECOMMENDED)
+        id: '1221838968572743840' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1220313226240196658', // ← Your server ID
+        categoryId: '1221722482726146078', // ← The modmail category ID
+        staffRoles: ['1220314781878190110'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
